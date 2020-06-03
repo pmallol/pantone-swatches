a70b320cca26da64e683881c0eee17112bc3af84
pmallol <pmallol@manas.tech>
2020-06-03 01:06:45 -0300

    Pump up the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="about">
-    <h1>Hola!</h1>
+    <h1>Hola,</h1>
     <h2>Thanks for stopping by :)</h2>
+    <p>This is still a work in progress.</p>
+    <div class="contact"><a href="https://about.tokto.dev">Get in touch!</a></div>
   </div>
 </template>
 
 <style scoped>
-  h1 {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin: 1rem 0;
-  }
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 1rem 0;
+}
+
+.contact a {
+  color: #000000;
+  font-weight: bold;
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+}
+
+.contact a:hover { color: inherit }
+
+.contact a:hover::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: .1rem;
+  width: 100%;
+  background: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+  z-index: -1;
+}
+
+p { margin: 1rem 0;}
 </style>
