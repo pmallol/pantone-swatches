5a81ccf2310f48d27ed3ad76f07f71b11885e074
pmallol <pmallol@manas.tech>
2020-06-03 00:09:07 -0300

    Refactor App

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,97 @@
 <template>
   <div id="app">
-    <div class=header>
+    <header>
       <h1>PANTONE <span>swatches</span></h1>
       <div id="nav">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
       </div>
-    </div>
-    <router-view/>
-    <div class=footer>
-      <p>PANTONE colors thanks to <a href="https://github.com/vanderlee/colorpicker/wiki/Pantone-colors-(csv,-semicolon-separated)" target="_blank">@vanderlee</a></p>
-    </div>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      <p><b>PANTONE</b> colors codes thanks to <a href="https://github.com/vanderlee/colorpicker/wiki/Pantone-colors-(csv,-semicolon-separated)" target="_blank">@vanderlee</a></p>
+    </footer>
   </div>
 </template>
 
 <style>
-  #app {
-    font-family: Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #000000;
-    max-width: 80vw;
-    height: 100vh;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-  }
+#app {
+  background-color: #f7f7f7;
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #000000;
+  max-width: 100vw;
+  min-height: 100vh;
+  height: 100%;
+  margin: 0 auto;
+}
 
-  .header {
-    margin: 3rem 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
+#app > * {
+  max-width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+}
 
-  .header h1 {
-    font-size: 2.5rem;
-    font-weight: bold;
-    display: inline-flex;
-    flex-direction: column;
-  }
+header {
+  padding: 3rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+}
 
-  .header h1:hover {
-    color: transparent;
-    background-clip: text;
-    background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
-    cursor: default;
-  }
+header h1 {
+  font-size: 2.5rem;
+  font-weight: bold;
+  display: inline-flex;
+  flex-direction: column;
+}
 
-  .header h1 span {
-    letter-spacing: 1px;
-    font-size: 1.5rem;
-    font-weight: normal;
-  }
+header h1:hover {
+  color: transparent;
+  background-clip: text;
+  background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+  cursor: default;
+}
 
-  #nav a {
-    font-weight: bold;
-    color: #444444;
-    text-decoration: none;
-    margin: 0 1rem;
-  }
+header h1:hover span {
+  color: #000000;
+}
 
-  .footer {
-    font-size: .8rem;
-    padding: 2rem 0;
-    margin: 2rem 0;
-    margin-top:auto;
-  }
+header h1 span {
+  letter-spacing: 1px;
+  font-size: 1.5rem;
+  font-weight: normal;
+}
 
-  .footer a {
-    color: black;
-    text-decoration: none;
-  }
+#nav a {
+  font-weight: bold;
+  color: #444444;
+  text-decoration: none;
+  margin: 0 1rem;
+}
 
-  #nav a:hover,
-  #nav a.router-link-exact-active,
-  .footer a:hover {
-    color: #6945a9;
-  }
+main {
+  min-height: calc(100vh - 23rem);
+}
+
+footer {
+  font-size: .8rem;
+  text-align: center;
+  padding: 8rem 0 2rem;
+  height: calc(11 * 1rem);
+}
+
+footer a {
+  color: black;
+  text-decoration: none;
+}
+
+#nav a:hover,
+#nav a.router-link-exact-active,
+footer a:hover {
+  color: #f93a2b;
+}
 </style>
