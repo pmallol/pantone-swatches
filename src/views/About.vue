4e0a0765f11cdd443e0755fa238583e18098385f
pmallol <pmallol@manas.tech>
2020-06-02 23:13:22 -0300

    Update About view

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>Hola!</h1>
+    <h2>Thanks for stopping by :)</h2>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin: 1rem 0;
+  }
+</style>
